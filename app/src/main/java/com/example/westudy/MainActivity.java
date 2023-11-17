67bfdd2eaddc2449aec0e998a141cec146eea6d7
Afiq <muhdafiq197@gmail.com>
2023-11-17 12:29:56 +0800

    adding materials and do base things for hard skills section

--- app/src/main/java/com/example/westudy/MainActivity.java
+++ app/src/main/java/com/example/westudy/MainActivity.java
@@ -3,14 +3,24 @@ package com.example.westudy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.general_login_page);
-        setContentView(R.layout.mainpage_landing_page);
+        setContentView(R.layout.general_login_page);
+
+        Button BTNLogin = findViewById(R.id.BTNLogin);
+
+        BTNLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.mainpage_landing_page);
+            }
+        });
 
 
 
