92e8ccfb1e1dfdec29bdc2a10775ff10aea33102
Afiq <muhdafiq197@gmail.com>
2024-01-01 13:59:04 +0800

    add username and progress record in db

--- app/src/main/java/com/example/westudy/Model/UserModel.java
+++ app/src/main/java/com/example/westudy/Model/UserModel.java
@@ -3,15 +3,40 @@ package com.example.westudy.Model;
 import com.google.firebase.Timestamp;
 
 public class UserModel {
-    private String email,userID;
+    private String username,email,userID;
+    private int skillsProgress;
     private Timestamp createdTimeStamp;
 
     public UserModel() {
     }
-    public UserModel(String email, Timestamp createdTimeStamp,String userID) {
+//    public UserModel(String email, Timestamp createdTimeStamp,String userID) {
+//        this.email = email;
+//        this.createdTimeStamp = createdTimeStamp;
+//        this.userID = userID;
+//    }
+
+    public UserModel(String username, String email, String userID, Timestamp createdTimeStamp) {
+        this.username = username;
         this.email = email;
-        this.createdTimeStamp = createdTimeStamp;
         this.userID = userID;
+        this.skillsProgress = 0;
+        this.createdTimeStamp = createdTimeStamp;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String name) {
+        this.username = name;
+    }
+
+    public int getSkillsProgress() {
+        return skillsProgress;
+    }
+
+    public void setSkillsProgress(int skillsProgress) {
+        this.skillsProgress = skillsProgress;
     }
 
     public String getEmail() {
